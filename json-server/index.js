5119222028b85bf822a97604bc8b11be725e64f9
Dmitry Teleshev <teleshev120@mail.ru>
2024-08-16 01:41:47 +0300

    feat: #5

--- json-server/index.js
+++ json-server/index.js
@@ -18,6 +18,7 @@ server.use(async (req, res, next) => {
 
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
