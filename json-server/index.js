a7b03cc8ca1359dc3bd3d62246e9de03b52c7e66
Dmitry Teleshev <teleshev120@mail.ru>
2024-08-16 00:39:50 +0300

    #3

--- json-server/index.js
+++ json-server/index.js
@@ -1,27 +1,30 @@
-const jsonServer = require("json-server")
-const path = require("path")
+const jsonServer = require("json-server");
+const path = require("path");
+const fs = require("fs");
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-const router = jsonServer.router(path.resolve(__dirname, "db.json"))
+const db = JSON.parse(fs.readFileSync(path.join("db.json")));
+
+const router = jsonServer.router(db);
 
 // Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
 server.use(async (req, res, next) => {
-  await new Promise(res => {
-    setTimeout(res, 800)
-  })
-  next()
-})
+  await new Promise((res) => {
+    setTimeout(res, 800);
+  });
+  next();
+});
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
-server.use(router)
+server.use(router);
 
 // запуск сервера
 server.listen(8000, () => {
-  console.log("server is running on 8000 port")
-})
+  console.log("server is running on 8000 port");
+});
